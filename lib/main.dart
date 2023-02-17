fe01e047fb2df643e1ec72a82ec702624bbddbca
OnArap <onarap95@gmail.com>
2023-02-17 15:49:50 +0100

    add ColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "constant/color_scheme.dart";
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+      ),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
