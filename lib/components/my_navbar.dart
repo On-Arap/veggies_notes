a910cb1b5a6d5abb3a72d1649720136e21ed37c7
OnArap <onarap95@gmail.com>
2023-02-20 17:17:28 +0100

    add Widget swap, change navbar place

--- lib/components/my_navbar.dart
+++ lib/components/my_navbar.dart
@@ -1,46 +1,8 @@
 import 'package:flutter/material.dart';
 
-class MyNavbar extends StatefulWidget {
-  const MyNavbar({super.key});
-
-  @override
-  State<MyNavbar> createState() => _MyNavbarState();
-}
-
-class _MyNavbarState extends State<MyNavbar> {
-  int currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return NavigationBar(
-      onDestinationSelected: (int index) {
-        setState(() {
-          currentPageIndex = index;
-        });
-      },
-      selectedIndex: currentPageIndex,
-      destinations: const <Widget>[
-        NavigationDestination(
-          icon: Icon(Icons.waves),
-          label: 'Scroll',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.search),
-          label: 'Search',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.explore),
-          label: 'Explore',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.map),
-          label: 'Map',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
-    );
-  }
+class Destination {
+  const Destination(this.index, this.title, this.icon);
+  final int index;
+  final String title;
+  final IconData icon;
 }
