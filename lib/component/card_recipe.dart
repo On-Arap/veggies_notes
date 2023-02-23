47c1b75e3a7f292c1dd74ca084b38ee13a41a033
OnArap <onarap95@gmail.com>
2023-02-23 16:41:15 +0100

    CardRecipe is done mostly

--- lib/component/card_recipe.dart
+++ lib/component/card_recipe.dart
@@ -2,28 +2,62 @@ import 'package:flutter/material.dart';
 
 class CardRecipe extends StatelessWidget {
   String title;
-  Image image;
+  String imageUrl;
 
-  CardRecipe({super.key, required this.title, required this.image});
+  CardRecipe({super.key, required this.title, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        height: 200,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-              child: Text(title),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      decoration: BoxDecoration(
+        color: const Color(0xFF1e2a27),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6),
+            offset: const Offset(0.0, 10.0),
+            blurRadius: 10.0,
+            spreadRadius: -6.0,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+            child: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
-            SizedBox(
-                child: AspectRatio(
-              aspectRatio: 1,
-              child: image,
-            )),
-          ],
-        ),
+          ),
+          Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2),
+                  BlendMode.multiply,
+                ),
+                image: NetworkImage(imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,10 +13,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CardRecipe(
-        title: "Tartiflette vegetarienne",
-        image: Image.network("https://i0.wp.com/freethepickle.fr/wp-content/uploads/2021/09/Tartiflettes-lardons-de-champignons-2-of-6.jpg?resize=800%2C1000&ssl=1"),
+    return SafeArea(
+      child: Column(
+        children: [
+          CardRecipe(
+            title: "Tartiflette vegetarienne",
+            imageUrl: "https://i0.wp.com/freethepickle.fr/wp-content/uploads/2021/09/Tartiflettes-lardons-de-champignons-2-of-6.jpg",
+          ),
+          CardRecipe(
+            title: "Pantry Pasta with Vegan Cream Sauce",
+            imageUrl: "https://assets.bonappetit.com/photos/5e4c58a6cf2f580008eba058/1:1/w_1920%2Cc_limit/HLY_Vegan%2520Alfredo_16x9.jpg",
+          ),
+          CardRecipe(
+            title: "Creamy Vegan sun-dried tomato and broccolini gnocchi",
+            imageUrl: "https://img.taste.com.au/028Wi8mM/w643-h428-cfill-q90/taste/2020/02/vegan-creamy-sun-dried-tomato-and-spinach-gnocchi-158620-1.jpg",
+          ),
+        ],
       ),
     );
   }
