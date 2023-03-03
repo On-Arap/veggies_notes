7f668a0cb7e9e205d838b3eac69c2978e74a0512
OnArap <onarap95@gmail.com>
2023-03-03 17:28:58 +0100

    recipe page update

--- lib/pages/recipe_page.dart
+++ lib/pages/recipe_page.dart
@@ -33,23 +33,106 @@ class RecipePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 500,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              image: DecorationImage(
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.2),
-                  BlendMode.multiply,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                image: DecorationImage(
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.2),
+                    BlendMode.multiply,
+                  ),
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit.cover,
                 ),
-                image: NetworkImage(imageUrl),
-                fit: BoxFit.cover,
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 14.0),
+              child: Wrap(
+                spacing: 0.0, // gap between adjacent chips
+                runSpacing: 10.0, // gap between lines
+                children: [
+                  Container(
+                    width: (MediaQuery.of(context).size.width / 2) - 16,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.timer),
+                        SizedBox(width: 4.0),
+                        Text(
+                          "20min",
+                          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width / 2) - 16,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.difference),
+                        SizedBox(width: 4.0),
+                        Text(
+                          "easy",
+                          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width / 2) - 16,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.cut),
+                        SizedBox(width: 4.0),
+                        Text(
+                          "10min",
+                          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width / 2) - 16,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.terrain_outlined),
+                        SizedBox(width: 4.0),
+                        Text(
+                          "Veggie",
+                          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width / 2) - 16,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.fireplace),
+                        SizedBox(width: 4.0),
+                        Text(
+                          "10min",
+                          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Divider(
+                height: 10.0,
+                thickness: 2,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
