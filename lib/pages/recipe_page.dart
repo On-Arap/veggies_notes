452b0cd3b0eb6affa0734fb52f9cd05d6adb1712
OnArap <onarap95@gmail.com>
2023-03-08 16:26:33 +0100

    add recipe area

--- lib/pages/recipe_page.dart
+++ lib/pages/recipe_page.dart
@@ -68,6 +68,11 @@ class RecipePage extends StatelessWidget {
             ),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
               child: Divider(
                 height: 10.0,
                 thickness: 2,
