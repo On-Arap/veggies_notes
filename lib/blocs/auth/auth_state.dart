007ae40f481d99c38a654bb3ce5205fa300ee2be
OnArap <onarap95@gmail.com>
2023-06-20 17:23:30 +0200

    add new State Auth

--- lib/blocs/auth/auth_state.dart
+++ lib/blocs/auth/auth_state.dart
@@ -2,9 +2,11 @@ part of 'auth_bloc.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class AuthInitial extends AuthState {}
+
+class AuthLogged extends AuthState {}
